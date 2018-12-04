a049ea990a7f361ed5b102cef6fd3cfd01671a95
UmedjonD <lazerr_94@mail.ru>
2018-12-04 15:02:43 +0400

    add anotation

--- src/main/java/Entity.java
+++ src/main/java/Entity.java
@@ -11,9 +11,15 @@ public class Entity {
         this.m = m;
         //this.s = s;
     }
+    @Secured(name = 1)
     public synchronized  String someMethod(Boolean s, int i){
         System.out.println("this is " + s + " and " + i);
         return someMethod(s, i);
     }
+    @Secured(name = 2, type = "notStrict")
+    private void second(int f) {
+
+    }
+
 
 }
--- src/main/java/Secured.java
+++ src/main/java/Secured.java
@@ -1,4 +1,12 @@
-public class Secured {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Secured {
+    int name();
+    String type() default "strict";
 
 }
